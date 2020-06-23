0054d543ffde092d1eba1c1fa8438a9e624521c9
danil <danil@DESKTOP-B5464OT>
2020-06-23 12:09:59 +0500

    Добавьте файлы проекта.

---
+++ ООП-4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП_4
+{
+    class HealthLife
+    {
+        private int Life;
+
+        public int TemperaturePerson
+        {
+            get
+            {
+                return Life;
+            }
+            set
+            {
+                if (value < 34 || value > 41)
+                    Console.WriteLine("Вы умераете");
+
+                else
+                    Console.WriteLine("Ваша температура составляет  " + value);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите температуру");
+            int writeTemperature = Convert.ToInt32(Console.ReadLine());
+            HealthLife newTem = new HealthLife();
+
+            newTem.TemperaturePerson = writeTemperature;
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
